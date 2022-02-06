83396ae79b0b3d27ee1c39bf7237092a0ea39e0d
Anwesh43 <anweshthecool0@gmail.com>
2022-02-06 20:02:58 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -36,12 +36,62 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {
         w, 
         h
+    }
+}
+
+const maxScale = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const size : number = Math.min(w, h) / 10 
+    const background = 'indigo'
+    return {
+        parentStyle() {
+            const left = `${w / 2}px`
+            const top = `${h / 2 - (h / 2 - size / 2) * sc2}px`
+            return {
+                left, 
+                top, 
+                position 
+            }
+        },
+        lineStyle(i : number) {
+            const left = `${-size / 2 + size * i}px`
+            const top = `${size / 2}px`
+            const width = `${Math.min(w, h) / 90}px`
+            const height = `${size * sc1}px`
+            return {
+                position, 
+                left, 
+                width, 
+                height, 
+                top, 
+                background 
+            }
+        },
+        squareStyle() {
+            const left = `${-size / 2}px`
+            const top = `${-size / 8}px`
+            const width = `${size}px`
+            const height = `${size / 4}px`
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height 
+            }
+        }
     }
 }
